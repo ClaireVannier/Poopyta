8f2217790568fec557c70ee25d7dc5e3ef2550d1
Claire Vannier <clairevannierpro@gmail.com>
2024-06-29 22:59:11 +0200

    finish website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, RouterLink, HeaderComponent, FooterComponent]
+    imports: [RouterOutlet, RouterLink, HeaderComponent, FooterComponent, HttpClientModule, FormsModule, ContactComponent]
 })
 export class AppComponent {
   title = 'Poopyta';
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,18 +1,34 @@
-// src/app/contact/contact.component.ts
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
+  imports: [FormsModule, CommonModule]
 })
 export class ContactComponent {
+  selectedFiles: File[] = [];
 
-  onSubmit() {
-    alert('Message envoyé!');
+  constructor() {}
+
+  onFileChange(event: any) {
+    if (event.target.files && event.target.files.length) {
+      this.selectedFiles = Array.from(event.target.files);
+    }
+  }
+
+  onSubmit(contactForm: NgForm) {
+    if (contactForm.valid) {
+      // Here you can handle the form submission.
+      // For now, we'll just log the form values and files to the console.
+      console.log('Form values:', contactForm.value);
+      console.log('Selected files:', this.selectedFiles);
+      alert('Form submitted successfully!');
+      contactForm.reset();
+      this.selectedFiles = [];
+    }
   }
 }
