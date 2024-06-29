820a50b53515d7f95aae2ce522c1c1757941a8eb
Claire Vannier <clairevannierpro@gmail.com>
2024-06-29 21:31:16 +0200

    finish structure of website

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,18 @@
+// src/app/contact/contact.component.ts
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
 
+  onSubmit() {
+    alert('Message envoyé!');
+  }
 }
