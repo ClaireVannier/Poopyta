ef2e360fe00831cda345eae9d0ee4e23b7d03dd8
Claire Vannier <clairevannierpro@gmail.com>
2024-06-30 22:16:26 +0200

    finish responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 // src/app/app.routes.ts
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
   { path: 'gallery', component: GalleryComponent },
   { path: 'contact', component: ContactComponent },
 ];
