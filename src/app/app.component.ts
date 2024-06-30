013743acab205f6d0aaa1b563313675c7d379f8f
Claire Vannier <clairevannierpro@gmail.com>
2024-06-30 15:44:05 +0200

    Moved Google Fonts imports to index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { ContactComponent } from './components/contact/contact.component';
-
+import { RouterOutlet } from '@angular/router';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, RouterLink, HeaderComponent, FooterComponent, HttpClientModule, FormsModule]
+    imports: [HeaderComponent, FooterComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'Poopyta';
