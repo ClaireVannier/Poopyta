a914dc21c0cbf191d3777981eda87c8073482c86
Claire Vannier <clairevannierpro@gmail.com>
2024-07-01 13:38:13 +0200

    change logo and header

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router'; // Importation du module de routage
 import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
@@ -13,6 +14,8 @@ import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 export class ContactComponent implements OnInit {
   @ViewChild('contactForm') contactForm!: ElementRef;
 
+  constructor(private router: Router) { } // Injection du service de routage
+
   ngOnInit() {
     // Initialiser EmailJS avec votre clé publique
     emailjs.init('qhlWHk1VPtRPNhbql');
@@ -23,8 +26,11 @@ export class ContactComponent implements OnInit {
     emailjs.sendForm('service_fo1b36a', 'template_x2154ij', this.contactForm.nativeElement)
       .then((result: EmailJSResponseStatus) => {
         console.log('SUCCESS!', result.status, result.text);
+        alert('Votre message a bien été envoyé');
+        this.router.navigate(['/gallery']); // Redirection vers la page /gallery
       }, (error) => {
         console.error('FAILED...', error);
+        alert('L\'envoi du message a échoué, veuillez réessayer.');
       });
   }
 }
